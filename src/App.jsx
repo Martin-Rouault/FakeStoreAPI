8b514b768b23d9e9b239ed30dcfb0d2c16786103
Martin-Rouault <martin.rlt24@gmail.com>
2025-03-31 14:55:21 +0200

    Add updateProduct function

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,29 @@ function App() {
         }
     }
 
+    async function updateProduct(id) {
+        const response = await fetch(
+            `https://fakestoreapi.com/products/${id}`,
+            {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    title: "T-Shirt",
+                    price: 20,
+                    description: "Un t-shirt super cool.",
+                    category: "Haut",
+                    image: "https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_.jpg",
+                }),
+            }
+        );
+        const data = await response.json();
+        if (data) {
+            alert(`Le produit avec l'id ${data.id} a été modifié`);
+        }
+    }
+
     return (
         <>
             <Button className="m-3" onClick={() => addProduct()}>
@@ -54,7 +77,7 @@ function App() {
                                         alt={product.title}
                                         className="card-img"
                                     />
-                                    <Card.Body>
+                                    <Card.Body className="d-flex flex-column">
                                         <Card.Title>{product.title}</Card.Title>
                                         <Card.Text>
                                             {product.description}
@@ -62,6 +85,15 @@ function App() {
                                         <p variant="primary" disabled>
                                             {product.price} €
                                         </p>
+                                        <div className="mt-auto">
+                                            <Button
+                                                onClick={() =>
+                                                    updateProduct(product.id)
+                                                }
+                                            >
+                                                Modifier le produit complet
+                                            </Button>
+                                        </div>
                                     </Card.Body>
                                 </Card>
                             </Col>
