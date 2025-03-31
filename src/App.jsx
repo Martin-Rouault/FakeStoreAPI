db81daa41a7dd55c41578aa7d0c79cf4c70d0841
Martin-Rouault <martin.rlt24@gmail.com>
2025-03-31 11:51:24 +0200

    post request to add a new product

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,35 @@ function App() {
         fetchProducts();
     }, []);
 
-    function addProduct() {
-        alert(`Produit ajouté`);
+    console.log(products);
+
+    const newProduct = {
+        id: 0,
+        title: "T-Shirt",
+        price: 20,
+        description: "Un t-shirt super cool.",
+        category: "Haut",
+        image: "https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_.jpg",
+    };
+
+    async function addProduct() {
+        const response = await fetch("https://fakestoreapi.com/products", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                title: "T-Shirt",
+                price: 20,
+                description: "Un t-shirt super cool.",
+                category: "Haut",
+                image: "https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_.jpg",
+            }),
+        });
+        const data = await response.json();
+        if (data) {
+            alert(`Le produit avec l'id ${data.id} a été créé`);
+        }
     }
 
     return (
